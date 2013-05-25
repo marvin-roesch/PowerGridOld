d8238ed21ba7effb0f7d6d3f910a9d1bc91c330a
PaleoCrafter <paleocraftmc@gmail.com>
2013-05-25 21:46:17 +0200

    Added some basic files
    Added the main package and the main file
    Added the license

---
+++ powergrid_src/de/paleocrafter/powergrid/PowerGrid.java
@@ -0,0 +1,15 @@
+package de.paleocrafter.powergrid;
+
+/**
+ *
+ * PowerGrid
+ *
+ * PowerGrid
+ *
+ * @author PaleoCrafter
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+public class PowerGrid {
+    
+}
